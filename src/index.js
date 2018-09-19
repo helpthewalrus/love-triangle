bd117f91e46b6e0daf8b75db2cec31d17df4dd8b
helpthewalrus <alinatylets@gmail.com>
2018-09-19 21:30:36 +0300

    task_2_commit_1

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  n = 0
+  for (i = 1; i < preferences.length + 1; i++) {
+    // checking that first number is not equal to its index and less than length of array
+    if (preferences[i-1] !== i && preferences[i-1] <= preferences.length) {
+      // checking that second number with index = first number is not equal to its index and less than length of array
+      if (preferences[preferences[i - 1] - 1] !== i && preferences[preferences[i - 1] - 1] <=  preferences.length) {
+        // checking that third number with index = second number is equal to index of the first number 
+        if (preferences[preferences[preferences[i - 1] - 1] - 1] === i) {
+          n ++;
+        }
+      }
+    }
+  }
+  return n/3;
 };
+
